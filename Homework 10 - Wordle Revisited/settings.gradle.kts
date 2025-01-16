08234dcc7b2fa86c3523c205eaf8eb3971f8a6b6
Bruno Gracia <153111961+BGracia05@users.noreply.github.com>
2025-01-15 21:20:32 -0800

    Delete Homework 10 - Wordle Revisited directory

--- Homework 10 - Wordle Revisited/settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-rootProject.name = "homework10"
-
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        mavenLocal()
-    }
-}
--- Homework 10 - Wordle Revisited/src/main/kotlin/SupportCode.kt
+++
@@ -1,7 +0,0 @@
-import java.io.File
-
-/**
- * Reads words from [filename], returning a list of the words.
- */
-fun readWordsFromFile(filename: String): List<String> =
-    File("data/dictionaries/$filename").readLines()
